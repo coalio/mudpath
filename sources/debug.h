b84740825d88ae52803649b2da586fc1418e40c0
Coal <dibashtori@gmail.com>
2021-12-25 22:11:46 -0600

    ??

--- sources/debug.h
+++ sources/debug.h
@@ -1,1 +1,7 @@
-#define DEBUG(msg) std::cerr << msg << std::endl;
+#define LINENUM 1
+// print line number
+#if LINENUM == 1
+    #define DEBUG(msg) std::cerr << "[" << __LINE__ << "] " msg << std::endl;
+#else
+    #define DEBUG(msg) std::cerr << msg << std::endl;
+#endif
